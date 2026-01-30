5d3776b2980d9e931852fddc6936c00342d62954
Vadym Raksha <hello@raksha.bio>
2026-01-30 22:57:49 +0100

    Update linctl.rb

--- Formula/linctl.rb
+++ Formula/linctl.rb
@@ -1,15 +1,15 @@
 class Linctl < Formula
   desc "Comprehensive command-line interface for Linear's API"
-  homepage "https://github.com/dorkitude/linctl"
-  url "https://github.com/dorkitude/linctl/archive/refs/tags/v0.1.1.tar.gz"
-  sha256 "5b56d129fe230504e7f68c7d69958a40d9575971c1d72eb6cad9d672027ae304"
+  homepage "https://github.com/hasdfa/linctl"
+  url "https://github.com/hasdfa/linctl/archive/refs/tags/v0.1.2.tar.gz"
+  sha256 "d5558cd419c8d46bdc958064cb97f963d1ea793866414c025906ec15033512ed"
   license "MIT"
-  head "https://github.com/dorkitude/linctl.git", branch: "master"
+  head "https://github.com/hasdfa/linctl.git", branch: "master"
 
   depends_on "go" => :build
 
   def install
-    system "go", "build", *std_go_args(ldflags: "-s -w -X github.com/dorkitude/linctl/cmd.version=#{version}")
+    system "go", "build", *std_go_args(ldflags: "-s -w -X github.com/hasdfa/linctl/cmd.version=#{version}")
   end
 
   test do
